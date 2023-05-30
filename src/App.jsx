1b594f427b29815addb44e3561e82732454ac8d9
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-30 16:06:51 +0100

    fix import statement

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/articles" element={<ArticleList />} />
-          <Route path="/articles/:articleId" element={<SingleArticle />} />
+          <Route path="/articles/:article_id" element={<SingleArticle />} />
         </Routes>
       </>
     </BrowserRouter>

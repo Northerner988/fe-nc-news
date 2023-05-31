9e64931f45bfe815ba0311bde0b732e1afef12bf
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-31 08:47:20 +0100

    fix loading

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { formatDate } from "../../utils/dateFormat";
 
 export default function ArticleCard({
   title,
@@ -9,12 +10,10 @@ export default function ArticleCard({
   comments,
   article_id,
 }) {
-  const formattedDate = new Date(created_at).toLocaleDateString();
-
   return (
     <Link key={article_id} to={`/articles/${article_id}`}>
       <article className="article-card">
-        <p className="article-date">Created on: {formattedDate}</p>
+        <p className="article-date">Created on: {formatDate(created_at)}</p>
         <img src={image} alt={title} className="article-image" />
         <h2 className="article-title">{title}</h2>
         <p className="article-votes">{votes} votes</p>

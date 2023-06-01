cb7e9af8e47d2d25139080ea950e0d2d583149fd
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-01 09:17:39 +0100

    add logic to account for when the article does not have any comments

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -14,10 +14,14 @@ export default function CommentsList({ article_id }) {
     });
   }, [article_id]);
 
+  if (isLoading) {
+    return <p>Fetching comments...</p>;
+  }
+
   return (
     <ul className="comment-container">
-      {isLoading ? (
-        <p>Fetching comments...</p>
+      {currentComments.length < 0 ? (
+        <li>This article has no comments, be the first to add something!</li>
       ) : (
         currentComments.map((comment) => (
           <CommentCard key={comment.comment_id} comment={comment.body} />

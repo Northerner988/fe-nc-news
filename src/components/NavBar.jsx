5f943a0285fb6c5d2dbfb11cd68247b9045945cb
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-01 08:30:38 +0100

    fix formating

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Navigation() {
+export default function NavBar() {
   return (
     <nav className="nav">
       <Link to="/">Home</Link>

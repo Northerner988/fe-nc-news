c0dc961875f80c4f974be97fb16106f64e2d4b43
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-01 13:32:40 +0100

    change voting

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -10,33 +10,48 @@ export default function updateArticleVotes({ article_id, votes }) {
   const [error, setError] = useState(null);
 
   function incVotes() {
-    setCurrentVotes((currVotes) => currVotes + 1);
-    setHasVoted(true);
-    patchArticleVotes(article_id, 1).catch((err) => {
+    if (!hasVoted) {
+      setCurrentVotes((currVotes) => currVotes + 1);
+      setHasVoted(1);
+      patchArticleVotes(article_id, 1).catch((err) => {
+        setCurrentVotes((currVotes) => currVotes - 1);
+        setHasVoted(false);
+        setError(err);
+      });
+    } else {
       setCurrentVotes((currVotes) => currVotes - 1);
       setHasVoted(false);
-      setError(err);
-    });
+      patchArticleVotes(article_id, -1).catch((err) => {
+        setCurrentVotes((currVotes) => currVotes + 1);
+        setHasVoted(true);
+        setError(err);
+      });
+    }
   }
 
   function decVotes() {
-    setCurrentVotes((currVotes) => currVotes - 1);
-    setHasVoted(true);
-    patchArticleVotes(article_id, -1).catch((err) => {
+    if (!hasVoted) {
+      setCurrentVotes((currVotes) => currVotes - 1);
+      setHasVoted(true);
+      patchArticleVotes(article_id, -1).catch((err) => {
+        setCurrentVotes((currVotes) => currVotes + 1);
+        setHasVoted(false);
+        setError(err);
+      });
+    } else {
       setCurrentVotes((currVotes) => currVotes + 1);
       setHasVoted(false);
-      setError(err);
-    });
+      patchArticleVotes(article_id, 1).catch((err) => {
+        setCurrentVotes((currVotes) => currVotes - 1);
+        setHasVoted(true);
+        setError(err);
+      });
+    }
   }
 
   return (
     <div className="article-votes">
-      <IconButton
-        aria-label="add"
-        size="large"
-        onClick={incVotes}
-        disabled={hasVoted}
-      >
+      <IconButton aria-label="add" size="large" onClick={incVotes}>
         <AddRoundedIcon />
       </IconButton>{" "}
       <p>{currentVotes} Votes </p>
@@ -45,7 +60,6 @@ export default function updateArticleVotes({ article_id, votes }) {
         size="large"
         variant="outlined"
         onClick={decVotes}
-        disabled={hasVoted}
       >
         <RemoveRoundedIcon />
       </IconButton>

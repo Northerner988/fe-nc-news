ae0fc96703efaf007611d722aea0e7f77c292c3c
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-02 09:21:56 +0100

    make articles on homepage linkable to their own single page

--- src/components/LatestArticleCard.jsx
+++ src/components/LatestArticleCard.jsx
@@ -1,10 +1,20 @@
-export default function LatestArticleCard({ title, image, author, topic }) {
+import { Link } from "react-router-dom";
+
+export default function LatestArticleCard({
+  title,
+  image,
+  author,
+  topic,
+  article_id,
+}) {
   return (
-    <article className="latest-article-card">
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>Published by: {author}</p>
-      <p>{topic}</p>
-    </article>
+    <Link to={`/articles/${article_id}`}>
+      <article className="latest-article-card">
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>Published by: {author}</p>
+        <p>{topic}</p>
+      </article>
+    </Link>
   );
 }

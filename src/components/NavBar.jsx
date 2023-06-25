89ef03ba28d2ef07212b113896b122cb1cb31a77
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-25 22:46:19 +0100

    add logo to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { ImNewspaper } from "react-icons/im";
 
 export default function NavBar() {
   return (
     <nav className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/articles">Articles</Link>
+      <div className="nav__logo">
+        <ImNewspaper className="logo" />
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
